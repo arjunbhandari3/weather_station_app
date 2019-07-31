c41bab8f6bae0cb1e07ff8cfd608f281e6ddd43c
Arjun Bhandari <arjunbhandari3773@gmail.com>
2019-07-31 15:41:24 +0545

    Update constants.dart

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -2,6 +2,13 @@ class CONSTANTS {
   static const SHARED_PREF_KEY_TEMPERATURE_UNIT = "temp_unit";
 }
 
+enum AlertAction {
+  cancel,
+  discard,
+  disagree,
+  agree,
+}
+
 const String apiURL = "https://reqres.in/api/users/2";
 const bool devMode = false;
 const double textScaleFactor = 1.0;
