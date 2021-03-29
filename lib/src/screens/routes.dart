b033e4c0e6047fa973bc06db99f8953edce12fff
Arjun Bhandari <arjunbhandari3773@gmail.com>
2021-03-29 18:42:41 +0545

    Update routes.dart

--- lib/src/screens/routes.dart
+++ lib/src/screens/routes.dart
@@ -3,7 +3,6 @@ import 'package:weather_station/src/screens/home_screen.dart';
 import 'package:weather_station/src/screens/settings_screen.dart';
 import 'package:weather_station/src/screens/weather_screen.dart';
 import 'package:weather_station/src/screens/check_weather.dart';
-import 'package:weather_station/src/screens/login.dart';
 
 class Routes {
   static final mainRoute = <String, WidgetBuilder>{
@@ -11,6 +10,5 @@ class Routes {
     '/weatherscreen': (context) => WeatherScreen(),
     '/settings': (context) => SettingsScreen(),
     '/checkweather': (context) => SearchWeatherScreen(),
-    '/login': (BuildContext context) => LoginPage(),
   };
 }
